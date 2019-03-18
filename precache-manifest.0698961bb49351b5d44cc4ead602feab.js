c860de3c93f6852c801466f004ff61a7c3fcd88f
Aryna Zaiko <arishazaiko@gmail.com>
2019-03-19 01:29:09 +0300

    Updates

---
+++ precache-manifest.0698961bb49351b5d44cc4ead602feab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "83007b11cf2b37a3d196",
+    "url": "/songs/static/js/runtime~main.83007b11.js"
+  },
+  {
+    "revision": "c86e476946dfc5115861",
+    "url": "/songs/static/js/main.c86e4769.chunk.js"
+  },
+  {
+    "revision": "f5ef12de7d7d1af0c973",
+    "url": "/songs/static/js/2.f5ef12de.chunk.js"
+  },
+  {
+    "revision": "c86e476946dfc5115861",
+    "url": "/songs/static/css/main.3a9ec456.chunk.css"
+  },
+  {
+    "revision": "f5ef12de7d7d1af0c973",
+    "url": "/songs/static/css/2.36bf82e0.chunk.css"
+  },
+  {
+    "revision": "18a4bc43bc274ad7fb99f6cf7fed7317",
+    "url": "/songs/index.html"
+  }
+];
